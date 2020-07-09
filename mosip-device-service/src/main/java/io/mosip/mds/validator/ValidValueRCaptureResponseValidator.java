feae7d5ee7744fbc4488deb9774a8fb83110dce6
U156129 <>
2020-07-09 21:14:10 +0530

    signature validations added

--- mosip-device-service/src/main/java/io/mosip/mds/validator/ValidValueRCaptureResponseValidator.java
+++ mosip-device-service/src/main/java/io/mosip/mds/validator/ValidValueRCaptureResponseValidator.java
@@ -1,16 +1,19 @@
 package io.mosip.mds.validator;
 
+import java.io.IOException;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import org.jose4j.lang.JoseException;
 import org.springframework.util.ObjectUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.mosip.mds.dto.CaptureResponse;
 import io.mosip.mds.dto.CaptureResponse.CaptureBiometric;
 import io.mosip.mds.dto.CaptureResponse.CaptureBiometricData;
-import io.mosip.mds.dto.RegistrationCaptureResponse;
-import io.mosip.mds.dto.RegistrationCaptureResponse.RegistrationCaptureBiometric;
 import io.mosip.mds.dto.ValidateResponseRequestDto;
 import io.mosip.mds.entitiy.Validator;
 
@@ -18,7 +21,7 @@ public class ValidValueRCaptureResponseValidator extends Validator{
 
 	private final List<String> bioSubTypeFingerList= getBioSubTypeFinger();
 	private final List<String> bioSubTypeIrisList = getBioSubTypeIris();
-
+	private static ObjectMapper mapper=new ObjectMapper();;
 	public ValidValueRCaptureResponseValidator() {
 		super("ValidValueRCaptureResponseValidator", "Valid Value Registration Capture Response Validator");
 	}
@@ -31,6 +34,10 @@ public class ValidValueRCaptureResponseValidator extends Validator{
 			errors.add("Response is empty");
 			return errors;
 		}
+		
+		errors=validateCaptureSignatureTampered(response, errors);
+		if(errors.size()!=0)return errors;
+
 		// Check for Biometrics block
 		CaptureResponse registrationCaptureResponse = (CaptureResponse) response.getMdsDecodedResponse();
 
@@ -47,19 +54,21 @@ public class ValidValueRCaptureResponseValidator extends Validator{
 				errors=validateActualValueDatadecoded(errors, dataDecoded);
 				if(errors.size()!=0)return errors;
 				//TODO check for env
+				if( !dataDecoded.env.equals(CommonConstant.STAGING) && !dataDecoded.env.equals(CommonConstant.DEVELOPER)
+						&& !dataDecoded.env.equals(CommonConstant.PRE_PRODUCTION) && !dataDecoded.env.equals(CommonConstant.PRODUCTION))
+				{
+					errors.add("Capture response biometrics-dataDecoded env is invalid");
+					return errors;
+				}
 
 				//TODO check time stamp for ISO Format date time with timezone
 				errors=CommonValidator.validateTimeStamp(dataDecoded.timestamp,errors);
 				if(errors.size()!=0)return errors;
 
 				//TODO check for requestedScore
-
 				//TODO check for quality score
 			}
-
 		}
-
-
 		return errors;
 	}
 	private List<String> validateActualValueDatadecoded(List<String> errors, CaptureBiometricData dataDecoded) {
@@ -81,8 +90,6 @@ public class ValidValueRCaptureResponseValidator extends Validator{
 			errors.add("Registration Capture response biometrics-dataDecoded purpose is invalid");
 			return errors;
 		}
-
-		//TODO Check for digitalId dataDecoded.digitalId
 
 		return errors;
 	}
@@ -110,6 +117,7 @@ public class ValidValueRCaptureResponseValidator extends Validator{
 			return errors;
 		}
 
+		//TODO Check for digitalId dataDecoded.digitalId
 		errors=validateDigitalId(dataDecoded, errors);
 		return errors;
 	}
@@ -120,29 +128,54 @@ public class ValidValueRCaptureResponseValidator extends Validator{
 		return errors;
 	}
 
-
 	public List<String> getBioSubTypeIris() {
 		List<String> bioSubTypeIrisList = new ArrayList<String>();
-		bioSubTypeIrisList.add("Left");
-		bioSubTypeIrisList.add("Right");
-		bioSubTypeIrisList.add( "UNKNOWN");
+		bioSubTypeIrisList.add(CommonConstant.LEFT);
+		bioSubTypeIrisList.add(CommonConstant.RIGHT);
+		bioSubTypeIrisList.add( CommonConstant.UNKNOWN);
 		return bioSubTypeIrisList;
 	}
 	public List<String> getBioSubTypeFinger() {
 		List<String> bioSubTypeFingerList=new ArrayList<String>();
-		bioSubTypeFingerList.add("Left IndexFinger");
-		bioSubTypeFingerList.add("Left MiddleFinger");
-		bioSubTypeFingerList.add("Left RingFinger");
-		bioSubTypeFingerList.add("Left LittleFinger");
-		bioSubTypeFingerList.add("Left Thumb");
-		bioSubTypeFingerList.add("Right IndexFinger");
-		bioSubTypeFingerList.add("Right MiddleFinger");
-		bioSubTypeFingerList.add("Right RingFinger");
-		bioSubTypeFingerList.add("Right LittleFinger");
-		bioSubTypeFingerList.add("Right Thumb");
-		bioSubTypeFingerList.add("UNKNOWN");
+		bioSubTypeFingerList.add(CommonConstant.LEFT_INDEX_FINGER);
+		bioSubTypeFingerList.add(CommonConstant.LEFT_MIDDLE_FINGER);
+		bioSubTypeFingerList.add(CommonConstant.LEFT_RING_FINGER);
+		bioSubTypeFingerList.add(CommonConstant.LEFT_LITTLE_FINGER);
+		bioSubTypeFingerList.add(CommonConstant.LEFT_THUMB);
+		bioSubTypeFingerList.add(CommonConstant.RIGHT_INDEX_FINGER);
+		bioSubTypeFingerList.add(CommonConstant.RIGHT_MIDDLE_FINGER);
+		bioSubTypeFingerList.add(CommonConstant.RIGHT_RING_FINGER);
+		bioSubTypeFingerList.add(CommonConstant.RIGHT_LITTLE_FINGER);
+		bioSubTypeFingerList.add(CommonConstant.RIGHT_THUMB);
+		bioSubTypeFingerList.add(CommonConstant.UNKNOWN);
 
 		return bioSubTypeFingerList;
+	}
+	private List<String> validateCaptureSignatureTampered(ValidateResponseRequestDto response, List<String> errors) {
+		CaptureResponse mdsResponse = null;
+		try {
+			mdsResponse = (CaptureResponse) (mapper.readValue(response.mdsResponse.getBytes(), CaptureResponse.class));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		for (CaptureResponse.CaptureBiometric biometric : mdsResponse.biometrics) {
+
+			if (biometric.getData() != null) {
+				try {
+					if(CommonValidator.validateSignature(biometric.getData())) {
+						errors.add("signature verification failed");
+						return errors;
+					}
+				} catch (CertificateException | JoseException | IOException e) {
+					errors.add("mdsResponse with Invalid Signature");
+					return errors;
+					//e.printStackTrace();
+				}
+			}
+		}
+		return errors;
 	}
 
 }
