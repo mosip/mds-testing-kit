17b9b7e2b207c286a04732720d6dbadaf91a7576
kiranrajbs <kiranraj.s@mindtree.com>
2021-01-20 17:15:24 +0530

    image changes added

--- mosip-device-service/src/main/java/io/mosip/mds/helper/FaceImageExtractor.java
+++ mosip-device-service/src/main/java/io/mosip/mds/helper/FaceImageExtractor.java
@@ -1,5 +1,8 @@
 package io.mosip.mds.helper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
@@ -7,38 +10,40 @@ import java.util.List;
 
 public class FaceImageExtractor {
 
+    private static final Logger logger = LoggerFactory.getLogger(FaceImageExtractor.class);
+
     public List<ExtractDTO> extractFaceImageData(byte[] decodedBioValue) {
         List<ExtractDTO> extracts = new ArrayList<>();
 
         try(DataInputStream din = new DataInputStream(new ByteArrayInputStream(decodedBioValue))) {
-            System.out.println("Started processing bio value : " + new Date());
+            logger.info("Started processing bio value : {}", new Date());
 
             //Parsing general header
             byte[] format = new byte[4];
             din.read(format, 0, 4);
-            System.out.println("format >>>>>>>>>" + new String(format));
+            logger.debug("format >>>>>>>>> {} ",new String(format));
 
             byte[] version = new byte[4];
             din.read(version, 0, 4);
-            System.out.println("version >>>>>>>>>" + new String(version));
+            logger.debug("version >>>>>>>>> {}", new String(version));
 
             int recordLength = din.readInt(); // 4 bytes
-            System.out.println("recordLength >>>>>>>>>" + recordLength);
+            logger.debug("recordLength >>>>>>>>>" + recordLength);
 
             short numberofRepresentionRecord = din.readShort();
-            System.out.println("numberofRepresentionRecord >>>>>>>>>" + numberofRepresentionRecord);
+            logger.debug("numberofRepresentionRecord >>>>>>>>>" + numberofRepresentionRecord);
 
             //NOTE: No certification schemes are available for this part of ISO/IEC 19794.
             byte certificationFlag = din.readByte();
-            System.out.println("certificationFlag >>>>>>>>>" + certificationFlag);
+            logger.debug("certificationFlag >>>>>>>>>" + certificationFlag);
 
             byte[] temporalSequence = new byte[2];
             din.read(temporalSequence, 0, 2);
-            System.out.println("temporalSequence >>>>>>>>>" + new String(temporalSequence));
+            logger.debug("temporalSequence >>>>>>>>>" + new String(temporalSequence));
 
             //Parsing representation header
             int representationLength = din.readInt();
-            System.out.println("representationLength >>>>>>>>>" + representationLength);
+            logger.debug("representationLength >>>>>>>>>" + representationLength);
 
             byte[] representationData = new byte[representationLength-4];
             din.read(representationData, 0, representationData.length);
@@ -48,7 +53,7 @@ public class FaceImageExtractor {
                 rdin.read(captureDetails, 0, 14);
 
                 byte noOfQualityBlocks = rdin.readByte();
-                System.out.println("noOfQualityBlocks >>>>>>>>>" + noOfQualityBlocks);
+                logger.debug("noOfQualityBlocks >>>>>>>>>" + noOfQualityBlocks);
 
                 if(noOfQualityBlocks > 0) {
                    byte[] qualityBlocks = new byte[noOfQualityBlocks * 5];
@@ -56,7 +61,7 @@ public class FaceImageExtractor {
                 }
 
                 short noOfLandmarkPoints = rdin.readShort();
-                System.out.println("noOfLandmarkPoints >>>>>>>>>" + noOfLandmarkPoints);
+                logger.debug("noOfLandmarkPoints >>>>>>>>>" + noOfLandmarkPoints);
                 //read next 15 bytes
                 byte[] facialInformation = new byte[15];
                 rdin.read(facialInformation, 0, 15);
@@ -68,7 +73,7 @@ public class FaceImageExtractor {
                 }
 
                 byte faceType = rdin.readByte();
-                System.out.println("faceType >>>>>>>>>" + faceType);
+                logger.debug("faceType >>>>>>>>>" + faceType);
 
                 //The (1 byte) Image Data Type field denotes the encoding type of the Image Data block
                 //JPEG 00 HEX
@@ -77,14 +82,14 @@ public class FaceImageExtractor {
                 //PNG 03 HEX
                 //Reserved by SC 37 for future use 04 HEX to FF HEX
                 byte imageDataType = rdin.readByte();
-                System.out.println("imageDataType >>>>>>>>>" + imageDataType);
+                logger.debug("imageDataType >>>>>>>>>" + imageDataType);
 
                 byte[] otherImageInformation = new byte[9];
                 rdin.read(otherImageInformation, 0, otherImageInformation.length);
 
                 //reading representationData -> imageData + 3d info + 3d data
                 int lengthOfImageData = rdin.readInt();
-                System.out.println("lengthOfImageData >>>>>>>>>" + lengthOfImageData);
+                logger.debug("lengthOfImageData >>>>>>>>>" + lengthOfImageData);
 
                 byte[] image = new byte[lengthOfImageData];
                 rdin.read(image, 0, lengthOfImageData);
@@ -93,7 +98,7 @@ public class FaceImageExtractor {
                         imageDataType > 0 && imageDataType < 3 ? "jp2" : "png", 1));
             }
         } catch (Exception ex) {
-            ex.printStackTrace();
+            logger.error("Error extracting face image from biovalue", ex);
         }
         return extracts;
     }
